99da214d33c76fe53176636f2f4d9bd218dde9ff
rizalbayyu <38995446+rizalbayyu@users.noreply.github.com>
2020-11-29 15:04:46 +0700

    Update InstallDocker.sh

--- tugas7/InstallDocker.sh
+++ tugas7/InstallDocker.sh
@@ -10,26 +10,25 @@ if [ $EUID -ne 0 ]; then
     exit 1
 else
 
-sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-
-sudo apt update && sudo apt install docker-ce
-sudo systemctl enable docker
-
-sudo usermod -aG docker ${USER}
-su - ${USER}
-id -nG
-
 cat << _EOF_
-Install Docker Compose
+Install Docker & Docker Compose
 ==============================================================
 _EOF_
 
+sudo apt update -y && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt update -y && sudo apt install docker-ce -y
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl enable docker
+docker network create -d bridge wordpress_network
 docker-compose --version
 sudo systemctl restart docker
-docker network create -d bridge wordpress_network
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
 
 fi
