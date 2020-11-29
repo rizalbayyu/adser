86bb3e016e5fc2da1cf7250ac568feba902f849b
rizalbayyu <38995446+rizalbayyu@users.noreply.github.com>
2020-11-29 13:30:15 +0700

    Update InstallDocker.sh

--- tugas7/InstallDocker.sh
+++ tugas7/InstallDocker.sh
@@ -10,11 +10,11 @@ if [ $EUID -ne 0 ]; then
     exit 1
 else
 
-sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt update -y && sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-sudo apt update && sudo apt install docker-ce
+sudo apt update -y && sudo apt install -y docker-ce
 sudo systemctl enable docker
 
 sudo usermod -aG docker ${USER}
